931f757d7d25da7f0a11192b1cbb4f9491866b95
yasper <yasperrus@gmail.com>
2026-04-09 02:13:03 +0300

    change REDIS_URL in settings.py one hour

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ REST_FRAMEWORK = {
 
 ASGI_APPLICATION = "config.asgi.application"
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
+REDIS_URL = os.environ.get("REDIS_URL")
 print(os.getenv("REDIS_URL"))
 CHANNEL_LAYERS = {
     "default": {
